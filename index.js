85fc0c6364d9c0d38799c6a9a3a31e87cacc2e2f
kadirkayas <46502681+kadirkayas@users.noreply.github.com>
2024-04-20 14:47:53 +0900

    1

--- index.js
+++ index.js
@@ -1,20 +1,130 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomDice="dice"+randomNumber1+".png";
-var randomSource="./images/"+randomDice;
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomSource);
+$("button").click(function(){
+    var dice1 = drawDice()[0]
+    var dice2 = drawDice()[1]
+    $(".dice1").removeClass('start-dice');
+    $(".dice2").removeClass('start-dice');
+    if(dice1>dice2){
+        $(".title").text("WIN PLAYER ONE");
+    }
+    else if(dice1<dice2){
+        $(".title").text("WIN PLAYER TWO");
+    }
+    else{
+    }
 
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomDice="dice"+randomNumber2+".png";
-var randomSource="./images/"+randomDice;
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomSource);
+    switch (dice1) 
+    { 
+        case 1:
+            $(".dice1").html(htmlDice1)
+            break;
+        case 2:
+            $(".dice1").html(htmlDice2)
+            break;
+        case 3:
+            $(".dice1").html(htmlDice3)
+            break;
+        case 4:
+            $(".dice1").html(htmlDice4)
+            break;
+        case 5:
+            $(".dice1").html(htmlDice5)
+            break;
+        case 6:$(".dice1").html(htmlDice6)
+            break;
+    
+        default:
+            break;
+        }
 
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="Player 1 Wins"
+
+        switch (dice2) 
+    { 
+        case 1:
+            $(".dice2").html(htmlDice1)
+            break;
+        case 2:
+            $(".dice2").html(htmlDice2)
+            break;
+        case 3:
+            $(".dice2").html(htmlDice3)
+            break;
+        case 4:
+            $(".dice2").html(htmlDice4)
+            break;
+        case 5:
+            $(".dice2").html(htmlDice5)
+            break;
+        case 6:$(".dice2").html(htmlDice6)
+            break;
+    
+        default:
+            break;
+        }
+})
+
+
+function drawDice(){
+    var dice1,dice2
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2= Math.floor(Math.random() * 6) + 1;
+    return [dice1,dice2]
 }
-else{
-    document.querySelector("h1").innerHTML="Player 2 Wins"
-}
-
-
+var htmlDice1 = `
+<div class="one dice">
+<span class="dot"></span>
+</div>
+`;
+var htmlDice2 = `
+<div class="two dice">
+<span class="dot"></span>
+<span class="dot"></span>
+</div>
+`;
+var htmlDice3 = `
+<div class="tree dice">
+<span class="dot"></span>
+<span class="dot"></span>
+<span class="dot"></span>
+</div>
+`;
+var htmlDice4 = `
+<div class="fourth dice">
+<div class="column">
+    <span class="dot"></span>
+    <span class="dot"></span>
+</div>
+<div class="column">
+    <span class="dot"></span>
+    <span class="dot"></span>
+</div>
+</div>
+`;
+var htmlDice5 = `
+<div class="fifth dice">
+    <div class="column">
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+    <div class="center-column column">
+        <span class="dot"></span>
+    </div>
+    <div class="column">
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+</div>
+`;
+var htmlDice6 = `
+<div class="sixty dice">
+    <div class="column">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+    <div class="column">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+</div>
+`;
